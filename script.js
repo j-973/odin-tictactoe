2e4d4472e8bf0d10e6a5056088ee291fe3b989e3
j-973 <jvoorheis973@gmail.com>
2023-06-09 18:50:47 -0400

    created render() function in the gameBoard module to render the contents of the board array (Xes and Os) to the webpage/DOM.

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const gameBoard = (() => {
-    let board = ['', '', '',
-                '', '', '', 
-                '', '', ''];
+    let board = ['O', 'O', 'X',
+                'X', 'X', 'O', 
+                'O', 'X', 'X'];
+
+    //renders the contents of the board array to the webpage
+    render = () => {
+        let allSquares = document.querySelectorAll('.square');
+        allSquares.forEach((square, index) => {
+            square.textContent = board[index];
+        });
+    }
+    return { render };
 })();
+
+//returning the render function as a property of the object returned by the gameBoard module (enclosing it in curly braces) 
+//exposes the otherwise private render function to be accessed publicly outside of the gameBoard module"
+gameBoard.render();
