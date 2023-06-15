99803781d71d1aa2b48d4ef9ab732a5e4da1e9a6
j-973 <jvoorheis973@gmail.com>
2023-06-14 23:04:34 -0400

    Corrected render function declaration to use const in the gameBoard module. This makes render() properly private and inaccessible outside the module unless explicitly made public when returned in an object ( surrounded by {} ). Added background color change on mouse hover for board squares

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const gameBoard = (() => {
                 'O', 'X', 'X'];
 
     //renders the contents of the board array to the webpage
-    render = () => {
+    const render = () => {
         let allSquares = document.querySelectorAll('.square');
         allSquares.forEach((square, index) => {
             square.textContent = board[index];
@@ -13,6 +13,4 @@ const gameBoard = (() => {
     return { render };
 })();
 
-//returning the render function as a property of the object returned by the gameBoard module (enclosing it in curly braces) 
-//exposes the otherwise private render function to be accessed publicly outside of the gameBoard module"
 gameBoard.render();
