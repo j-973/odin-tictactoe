e7be9993bde2dfef9b696832f4cd3dfa93f0af32
j-973 <jvoorheis973@gmail.com>
2023-07-11 20:02:14 -0400

    added addMarker function to gameBoard module, which places a specified marker at a specified location on the gameboard, if that location is empty. tested using addMarker to place an X in the center of the gameBoard as game starts

--- script.js
+++ script.js
@@ -17,15 +17,28 @@ const gameBoard = (() => {
                 '', '', ''];
     }
 
-    return { render, clear };
+    //if the game board array's location is empty, assign a marker to that board index
+    //since board is an array, location corresponds to the index num of board (0 - 8 for this board with 9 spaces)
+    const addMarker = (marker, boardLocation) => {
+        if (board[boardLocation] === "") {
+            board[boardLocation] = marker;
+        }
+    }
+
+    return { render, clear, addMarker };
 })();
 
+//testing addMarker function by adding an X in the center of the gameboard
 const game = (() => {
     const start = () => {
         gameBoard.clear();
+        gameBoard.addMarker("X", 4);
+        gameBoard.render();
     }
 
 return { start }
 })();
 
 game.start();
+
+
